a44c7adfa8f2c25fa42e80a3331a2674b42fcd0e
Jonah Gräfe <jonah.greafe@gmail.com>
2025-01-01 23:56:10 +0100

    added verbose

--- src/game.py
+++ src/game.py
@@ -1,5 +1,5 @@
 class Game:
-    def __init__(self, agent_a, agent_b):
+    def __init__(self, agent_a, agent_b, verbose=0):
         self.payoff_matrix = {
             (0, 0): (3, 3),  # Both cooperate
             (0, 1): (0, 5),  # Player A cooperates, Player B defects
@@ -8,6 +8,7 @@ class Game:
         }
         self.agent_a = agent_a
         self.agent_b = agent_b
+        self.verbose = verbose
 
     def play_round(self):
         action_a = self.agent_a.choose_action()
@@ -17,6 +18,8 @@ class Game:
         self.agent_b.history.append(action_b)
         self.agent_a.reward_history.append(reward_a)
         self.agent_b.reward_history.append(reward_b)
+        if self.verbose >= 2:
+            print(f"{self.agent_a.__class__.__name__}: {action_a}, {self.agent_b.__class__.__name__}: {action_b} --> ({reward_a, reward_b}) ")
         return action_a, action_b, reward_a, reward_b
 
     def play_iterated_game(self, num_rounds):
@@ -28,4 +31,12 @@ class Game:
             self.agent_b.update(action_a)
             total_reward_a += reward_a
             total_reward_b += reward_b
+        if self.verbose >= 1:
+            print(f"{self.agent_a.__class__.__name__}: {self.agent_a.history} --> {self.agent_a.reward_history}\n{self.agent_b.__class__.__name__}: {self.agent_b.history} --> {self.agent_b.reward_history}")
+            if total_reward_a > total_reward_b:
+                print(f"{self.agent_a.__class__.__name__} wins against {self.agent_b.__class__.__name__} with {total_reward_a} to {total_reward_b}.")
+            elif total_reward_a < total_reward_b:
+                print(f"{self.agent_b.__class__.__name__} wins against {self.agent_a.__class__.__name__} with {total_reward_b} to {total_reward_a}.")
+            else:
+                print(f"{self.agent_a.__class__.__name__} ties against {self.agent_b.__class__.__name__} with {total_reward_a} to {total_reward_b}.")
         return total_reward_a, total_reward_b
